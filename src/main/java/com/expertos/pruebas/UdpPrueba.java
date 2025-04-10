5444613db0e0afd67ee6e72a07754b3cbb83b9ec
Israel Sánchez Cabrera <israsanchezcabrera@gmail.com>
2025-04-10 18:53:21 +0200

    Más archivos de prueba Creo que estos son todos los "recursos especiales" que necesitaremos para poder hacer el juego

--- src/main/java/com/expertos/pruebas/UdpPrueba.java
+++ src/main/java/com/expertos/pruebas/UdpPrueba.java
@@ -1,26 +1,69 @@
 package com.expertos.pruebas;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.*;
 
 public class UdpPrueba {
-    DatagramSocket server;
-    DatagramSocket client;
+    public static class ReceiveWithoutBlocking implements Runnable {
+        DatagramSocket socket;
+        byte[] buffer;
 
-    {
-        try {
-            server = new DatagramSocket(12099);
-            client = new DatagramSocket(12345);
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
+        public ReceiveWithoutBlocking(byte[] buffer, Integer port) throws SocketException {
+            this.socket = new DatagramSocket(port);
+            this.buffer = buffer;
+        }
+
+        @Override
+        public void run() {
+            DatagramPacket mensaje = new DatagramPacket(buffer, buffer.length);
+
+            try {
+                this.socket.receive(mensaje);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    byte[] buffer = "Hola mundo".getBytes();
+    public static void main(String[] args) {
+        DatagramSocket client;
 
-    DatagramPacket mesage = new DatagramPacket(buffer, buffer.length);
+        ObjectMapper mapper = new ObjectMapper();
+
+        String jsonContent = """
+                {
+                    "nombre": "Israel Sánchez Cabrera",
+                    "edad": "19",
+                    "email": "mecago"
+                }
+                """;
+        byte[] sendMessage = jsonContent.getBytes();
+
+        try {
+            client = new DatagramSocket();
+            byte[] response = new byte[100];
+
+            Thread receive = new Thread(new ReceiveWithoutBlocking(response, 12099));
+            receive.start();
+
+            DatagramPacket datagram = new DatagramPacket(
+                    sendMessage,
+                    sendMessage.length,
+                    InetAddress.getByName("127.0.0.1"),
+                    12099);
+
+            client.send(datagram);
+
+            receive.join();
+
+            System.out.println(mapper.readValue(new String(response), Persona.class));
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();;
+        }
+    }
 
 
 }
