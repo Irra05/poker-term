4383fade7b7967e278526d7aef496901e0079458
Israel Sánchez Cabrera <israsanchezcabrera@gmail.com>
2025-04-10 19:00:26 +0200

    Más archivos de prueba Creo que estos son todos los "recursos especiales" que necesitaremos para poder hacer el juego

---
+++ src/main/java/com/expertos/main/Main.java
@@ -0,0 +1,7 @@
+package com.expertos.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("poker-term");
+    }
+}
---
+++ src/main/java/com/expertos/poker/PokerGame.java
@@ -0,0 +1,7 @@
+package com.expertos.poker;
+
+public class PokerGame {
+    public static void main(String[] args) {
+        System.out.println("Poker Game");
+    }
+}
